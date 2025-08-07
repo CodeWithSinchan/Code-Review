3f8e9b3b40c788d84500ed1f842ea650928103c7
Shinchan <dkj98171@gmail.com>
2025-08-07 21:39:55 +0545

    yee

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const app = require('./src/app.js')
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server started on http://localhost:3000")
 })
